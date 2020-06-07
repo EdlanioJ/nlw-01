9956915bf8ecc0566dccc1bf8e403b640b134d6f
Edlâneo Júlio <edlanioj@gmail.com>
2020-06-07 15:18:33 +0100

    add README.md

---
+++ README.md
@@ -0,0 +1,69 @@
+<h1 align="center">
+    <img alt="Be the hero" title="#delicinha" src=".github/logo.svg" width="250px" />
+</h1>
+
+<h4 align="center">
+  🚀 Next Level Week #1 (Rocketseat)
+</h4>
+<p align="center">
+  <img alt="GitHub language count" src="https://img.shields.io/github/languages/count/EdlanioJ/nlw-01">
+
+  <img alt="Repository size" src="https://img.shields.io/github/repo-size/EdlanioJ/nlw-01">
+  
+  <a href="https://github.com/Rocketseat/semana-omnistack-10/commits/master">
+    <img alt="GitHub last commit" src="https://img.shields.io/github/last-commit/EdlanioJ/nlw-01">
+  </a>
+
+  <a href="https://github.com/Rocketseat/semana-omnistack-10/issues">
+    <img alt="Repository issues" src="https://img.shields.io/github/issues/EdlanioJ/nlw-01">
+  </a>
+
+  <img alt="License" src="https://img.shields.io/badge/license-MIT-brightgreen">
+</p>
+
+<p align="center">
+  <a href="#rocket-tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-projeto">Projeto</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-como-contribuir">Como contribuir</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#mortar_board-quem-ministrou">Quem ministrou?</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#memo-licença">Licença</a>
+</p>
+
+## :rocket: Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- [Node.js](https://nodejs.org/en/)
+- [React](https://reactjs.org)
+- [React Native](https://facebook.github.io/react-native/)
+- [Expo](https://expo.io/)
+
+- Bibliotecas Principais
+  - [Express](https://expressjs.com/)
+  - [KnexJS](http://knexjs.org/)
+  - [Celebrate](https://github.com/arb/celebrate)
+
+## 💻 Projeto
+
+O Ecoleta é um projeto que serve com uma conexão entre entitdade que coletam residuos organicos e não organicos há pessoas que desejam descartar esses residuos, esse projeto é totalmente voltado a fins ecológicos, foi desenvolvido de 1 a 7 de junho, na semana mundial do meio.
+
+## 🤔 Como contribuir
+
+- Faça um fork desse repositório;
+- Cria uma branch com a sua feature: `git checkout -b minha-feature`;
+- Faça commit das suas alterações: `git commit -m 'feat: Minha nova feature'`;
+- Faça push para a sua branch: `git push origin minha-feature`.
+
+Depois que o merge da sua pull request for feito, você pode deletar a sua branch.
+
+## :mortar_board: Quem ministrou?
+
+A semana foi ministrada por: [Diego Fernandes](https://github.com/diego3g) da Rocketseat
+
+## :memo: Licença
+
+Esse projeto está sob a licença MIT. Veja o arquivo [LICENSE](LICENSE.md) para mais detalhes.
+
+---
+
+Feito com ♥ by **Edlâneo Júlio**
--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,16 +1,18 @@
-import React, {useEffect, useState, ChangeEvent, FormEvent} from "react";
-import axios from "axios";
+import React, {
+  useEffect, useState, ChangeEvent, FormEvent,
+} from 'react';
+import axios from 'axios';
 
-import {Link, useHistory} from "react-router-dom";
-import { FiArrowLeft } from "react-icons/fi";
-import { Map, TileLayer, Marker } from "react-leaflet";
-import { LeafletMouseEvent } from "leaflet";
+import { Link, useHistory } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+import { Map, TileLayer, Marker } from 'react-leaflet';
+import { LeafletMouseEvent } from 'leaflet';
 
-import api from "../../services/api";
+import api from '../../services/api';
 
-import "./styles.css";
-import Dropzone from "../../components/Dropzone";
-import logo from "../../assets/logo.svg";
+import './styles.css';
+import Dropzone from '../../components/Dropzone';
+import logo from '../../assets/logo.svg';
 
 interface Item {
   id: number;
@@ -29,75 +31,77 @@ const CreatePoint = () => {
   const [items, setItems] = useState<Item[]>([]);
   const [states, setStates] = useState<string[]>([]);
   const [cities, setCities] = useState<string[]>([]);
-  const [selectedState, setSelectedState] = useState("0");
-  const [selectedCity, setSelectedCity] = useState("0");
+  const [selectedState, setSelectedState] = useState('0');
+  const [selectedCity, setSelectedCity] = useState('0');
   const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
   const [initialPosition, setInitialPosition] = useState<[number, number]>([0, 0]);
   const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    whatsapp: "",
+    name: '',
+    email: '',
+    whatsapp: '',
   });
   const [selectedItems, setSelectedItems] = useState<number[]>([]);
   const [selectedFile, setSelectedFile] = useState<File>();
   const history = useHistory();
   useEffect(() => {
-    api.get("items").then( response => {
-      setItems(response.data)
-    })
+    api.get('items').then((response) => {
+      setItems(response.data);
+    });
   }, []);
 
   useEffect(() => {
-    axios.get("https://www.universal-tutorial.com/api/getaccesstoken", {
-      headers: { 
-        "Accept": "application/json",
-        "api-token": process.env.API_TOKEN,
-        "user-email": process.env.API_USER_EMAIL
-      }
-    }).then( res => {
-      axios.get<IGetStatesResponse[]>("https://www.universal-tutorial.com/api/states/Angola", { headers: {
-        "Authorization": `Bearer ${res.data.auth_token}`,
-        "Accept": "application/json"
-      }}).then(data => {
-
-        const stateName = data.data.map(state => state.state_name);
+    axios.get('https://www.universal-tutorial.com/api/getaccesstoken', {
+      headers: {
+        Accept: 'application/json',
+        'api-token': process.env.API_TOKEN,
+        'user-email': process.env.API_USER_EMAIL,
+      },
+    }).then((res) => {
+      axios.get<IGetStatesResponse[]>('https://www.universal-tutorial.com/api/states/Angola', {
+        headers: {
+          Authorization: `Bearer ${res.data.auth_token}`,
+          Accept: 'application/json',
+        },
+      }).then((data) => {
+        const stateName = data.data.map((state) => state.state_name);
 
         setStates(stateName);
-      })
-    })
+      });
+    });
   }, []);
 
   useEffect(() => {
-    if(selectedState === "0") return;
+    if (selectedState === '0') return;
 
-    axios.get("https://www.universal-tutorial.com/api/getaccesstoken", {
-      headers: { 
-        "Accept": "application/json",
-        "api-token": process.env.API_TOKEN,
-        "user-email": process.env.API_USER_EMAIL
-      }
-    }).then( res => {
-      axios.get<IGetCityResponse[]>(`https://www.universal-tutorial.com/api/cities/${selectedState}`, { headers: {
-        "Authorization": `Bearer ${res.data.auth_token}`,
-        "Accept": "application/json"
-      }}).then(data => {
-
-        const cityName = data.data.map(city => city.city_name);
+    axios.get('https://www.universal-tutorial.com/api/getaccesstoken', {
+      headers: {
+        Accept: 'application/json',
+        'api-token': process.env.API_TOKEN,
+        'user-email': process.env.API_USER_EMAIL,
+      },
+    }).then((res) => {
+      axios.get<IGetCityResponse[]>(`https://www.universal-tutorial.com/api/cities/${selectedState}`, {
+        headers: {
+          Authorization: `Bearer ${res.data.auth_token}`,
+          Accept: 'application/json',
+        },
+      }).then((data) => {
+        const cityName = data.data.map((city) => city.city_name);
 
         setCities(cityName);
-      })
-    })
+      });
+    });
   }, [selectedState]);
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(position => {
-      const {latitude, longitude} =position.coords;
+    navigator.geolocation.getCurrentPosition((position) => {
+      const { latitude, longitude } = position.coords;
       setInitialPosition([
         latitude,
-        longitude
-      ])
-    })
-  }, [])
+        longitude,
+      ]);
+    });
+  }, []);
   function handleSetectState(event: ChangeEvent<HTMLSelectElement>) {
     const state = event.target.value;
 
@@ -113,72 +117,76 @@ const CreatePoint = () => {
   function handleMapClick(event: LeafletMouseEvent) {
     setSelectedPosition([
       event.latlng.lat,
-      event.latlng.lng
+      event.latlng.lng,
     ]);
   }
 
   function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
-    const {name, value} = event.target;
-    setFormData({ ...formData, [name]: value })
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
   }
 
   function handleSelectItem(id: number) {
-    const areadySelected = selectedItems.findIndex(item => item=== id)
-    if(areadySelected>= 0)  {
-      const filteredItems = selectedItems.filter(item => item !== id);
+    const areadySelected = selectedItems.findIndex((item) => item === id);
+    if (areadySelected >= 0) {
+      const filteredItems = selectedItems.filter((item) => item !== id);
 
       setSelectedItems(filteredItems);
-    }else {
+    } else {
       setSelectedItems([...selectedItems, id]);
     }
-    
   }
 
   async function handleSubmit(event: FormEvent) {
     event.preventDefault();
 
 
-    const {name, email, whatsapp} = formData;
-    const state= selectedState;
+    const { name, email, whatsapp } = formData;
+    const state = selectedState;
     const city = selectedCity;
-    const [latitude, longitude] = selectedPosition
-    const items = selectedItems;
+    const [latitude, longitude] = selectedPosition;
+    const itemsData = selectedItems;
 
     const data = new FormData();
-    
-      data.append("name", name);
-      data.append("email", email);
-      data.append("whatsapp", whatsapp);
-      data.append("state", state);
-      data.append("city", city);
-      data.append("latitude", String(latitude));
-      data.append("longitude", String(longitude));
-      data.append("items", items.join(","));
-      
-      if(selectedFile) {
-        data.append("image", selectedFile);
-      }
-   
 
-    await api.post("points", data);
+    data.append('name', name);
+    data.append('email', email);
+    data.append('whatsapp', whatsapp);
+    data.append('state', state);
+    data.append('city', city);
+    data.append('latitude', String(latitude));
+    data.append('longitude', String(longitude));
+    data.append('items', itemsData.join(','));
 
-    alert("ponto de colheta criado!");
+    if (selectedFile) {
+      data.append('image', selectedFile);
+    }
 
-    history.push("/");
+    await api.post('points', data);
+
+    alert('ponto de colheta criado!');
+
+    history.push('/');
   }
-  return(
+  return (
     <div id="page-create-point">
       <header>
         <img src={logo} alt="Ecoleta" />
 
         <Link to="/">
-          <FiArrowLeft/>
+          <FiArrowLeft />
           voltar par home
         </Link>
       </header>
 
       <form onSubmit={handleSubmit}>
-        <h1>Cadastro do <br/> ponto de coleta</h1>
+        <h1>
+          Cadastro do
+          {' '}
+          <br />
+          {' '}
+          ponto de coleta
+        </h1>
 
         <Dropzone onFileUploaded={setSelectedFile} />
         <fieldset>
@@ -236,14 +244,14 @@ const CreatePoint = () => {
           <div className="field-group">
             <div className="field">
               <label htmlFor="uf">Provinvia</label>
-              <select 
-                onChange={handleSetectState} 
-                value={selectedState} 
-                name="uf" 
+              <select
+                onChange={handleSetectState}
+                value={selectedState}
+                name="uf"
                 id="uf"
               >
                 <option value="0">Selecione uma provincia</option>
-                {states.map(state => (
+                {states.map((state) => (
                   <option key={state} value={state}>{state}</option>
                 ))}
               </select>
@@ -251,14 +259,14 @@ const CreatePoint = () => {
 
             <div className="field">
               <label htmlFor="city">Cidade</label>
-              <select 
-                onChange={handleSelectCity} 
+              <select
+                onChange={handleSelectCity}
                 value={selectedCity}
-                name="city" 
+                name="city"
                 id="city"
               >
                 <option value="0">Selecione uma cidade</option>
-                {cities.map(city => (
+                {cities.map((city) => (
                   <option value={city} key={city}>{city}</option>
                 ))}
               </select>
@@ -274,13 +282,13 @@ const CreatePoint = () => {
           </legend>
 
           <ul className="items-grid">
-            {items.map(item => (
-              <li 
-                key={item.id} 
+            {items.map((item) => (
+              <li
+                key={item.id}
                 onClick={() => handleSelectItem(item.id)}
-                className={selectedItems.includes(item.id) ? "selected" : ""}
+                className={selectedItems.includes(item.id) ? 'selected' : ''}
               >
-                <img src={item.image_url} alt={item.title}/>
+                <img src={item.image_url} alt={item.title} />
                 <span>{item.title}</span>
               </li>
             ))}
@@ -292,7 +300,7 @@ const CreatePoint = () => {
         </button>
       </form>
     </div>
-  )
-}
+  );
+};
 
 export default CreatePoint;
